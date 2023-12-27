0fd2002215fd300e1fe619c1514f86b2f0bfaba3
Ronnie Sheer <ronysheer@gmail.com>
2023-12-27 12:52:22 +0200

    01_02_begin

--- 01_02_begin/test_vision.py
+++ 01_02_begin/test_vision.py
@@ -17,10 +17,6 @@ response = client.chat.completions.create(
             "role": "user",
             "content": [
                 {
-                    "type": "text",
-                    "text": "Here is a mug. Create an short, appealing description for it for an e-commerce website. Output should only contain text.",
-                },
-                {
                     "type": "image_url",
                     "image_url": {
                         "url": f"data:image/jpeg;base64,{base64_image}",
