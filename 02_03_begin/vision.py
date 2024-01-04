9f08afcb5ff8076873ba51a336007803c4a3b68c
Ronnie Sheer <ronysheer@gmail.com>
2024-01-04 12:29:16 +0200

    reset file

--- 02_03_begin/vision.py
+++ 02_03_begin/vision.py
@@ -9,7 +9,7 @@ with open("refrigerator.png", "rb") as f:
     file_content = f.read()
     base64_image = base64.b64encode(file_content).decode("utf-8")
 
-PROMPT = """Which of the following items are missing from this grocery list? Soy Milk, Chocolate Pudding, Apples."""
+PROMPT = """"""
 
 response = client.chat.completions.create(
     model="gpt-4-vision-preview",
